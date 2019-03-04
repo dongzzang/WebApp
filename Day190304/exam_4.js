396bda8c03c071b26713f3c6c376a816d9bdb56d
dongzzang <dh134579@naver.com>
2019-03-04 19:23:12 +0900

    update exm

--- Day190304/exam_4.js
+++ Day190304/exam_4.js
@@ -7,29 +7,6 @@ function printDOM(node, prefix){
 }
 // printDOM(document, '');
 
-
-function highlightParas(containing){
-    if(typeof containing == 'string'){
-        containing = new RegExp(`\\b${containing}\\b`,'i');
-    }
-    const paras=document.getElementsByTagName('p');
-    console.log(paras);
-    for(let p of paras){
-        if(!containing.test(p.textContent)){
-            continue;
-        }
-        p.classList.add('highlight');
-    }
-}
-// highlightParas('unique');
-
-function removeParaHighlights(){
-  const paras = document.querySelectorAll('p.highlight');
-  for(let p of paras){
-    p.classList.remove('highlight');
-  }
-}
-
 let arr= new Array ();
 let i = 0;
 const insertValue = document.querySelectorAll('[data-action="submit"]');
@@ -53,6 +30,8 @@ for (let a of insertValue){
 console.log(insertValue);
 console.log(insertValue[0].dataset);
 
+
+
 const outputValue = document.querySelectorAll('[data-action="output"]');
 for (let a of outputValue){
   a.addEventListener('click', evt=>{
@@ -62,7 +41,11 @@ for (let a of outputValue){
     // document.getElementById(`이름 : ${arr[0].name} \n 점수 : ${arr[0].value}`)
       // document.write(`이름 : ${arr[j].name} \n 점수 : ${arr[j].value}\n`);
     for (let j=0;j<i;j++){
-      document.getElementById('output').append(`이름 : ${arr[j].name} 점수 : [${arr[j].value}] 총합 : ${arr[j].sum} 평균 : ${arr[j].avg}`);
+      const p1 = document.createElement('p');
+      const parent = document.getElementById('output');
+      p1.textContent = `이름 : ${arr[j].name} 점수 : [${arr[j].value}] 총합 : ${arr[j].sum} 평균 : ${arr[j].avg}`;
+      // document.getElementById('output').append(`이름 : ${arr[j].name} 점수 : [${arr[j].value}] 총합 : ${arr[j].sum} 평균 : ${arr[j].avg}`);
+      parent.appendChild(p1);
     }
 
 
@@ -70,16 +53,19 @@ for (let a of outputValue){
 }
 const rankoutputValue = document.querySelectorAll('[data-action="rankoutput"]');
 for (let a of rankoutputValue){
+
   a.addEventListener('click', evt=>{
     evt.preventDefault();
     console.log(rankoutputValue);
-    let arr2 = arr;
-
+    const arr2 = arr;
     arr2.sort(function(a, b) {
     return b.sum - a.sum ;});
-      document.getElementById('rankoutput').innerHTML = "";
+    document.getElementById('rankoutput').innerHTML = "";
     for (let j=0;j<i;j++){
-      document.getElementById('rankoutput').append(`이름 : ${arr2[j].name} 점수 : [${arr2[j].value}] 총합 : ${arr2[j].sum} 평균 : ${arr2[j].avg}`);
+      const p1 = document.createElement('p');
+      const parent = document.getElementById('rankoutput');
+      p1.textContent = `이름 : ${arr2[j].name} 점수 : [${arr2[j].value}] 총합 : ${arr2[j].sum} 평균 : ${arr2[j].avg}`;
+      parent.appendChild(p1);
     }
     // document.getElementById(`이름 : ${arr[0].name} \n 점수 : ${arr[0].value}`)
       // document.write(`이름 : ${arr[j].name} \n 점수 : ${arr[j].value}\n`);
@@ -91,35 +77,19 @@ for (let a of rankoutputValue){
 const findValue = document.querySelectorAll('[data-action="find"]');
 for (let a of findValue){
   a.addEventListener('click', evt=>{
+    let flag = 0;
     evt.preventDefault();
-    console.log(findValue);
     const paras=document.getElementsByTagName('input');
     console.log(paras[4].value);
     for(let j=0;j<i;j++){
       if(paras[4].value == arr[j].name){
         document.getElementById('findoutput').innerHTML = (`이름 : ${arr[j].name} 점수 : [${arr[j].value}] 총합 : ${arr[j].sum} 평균 : ${arr[j].avg}`);
+        flag = 1;
       }
+    }
+    if (flag == 0){
+      document.getElementById('findoutput').innerHTML = (`찾으시는 이름이 존재하지 않습니다..`);
     }
 
   });
 }
-//
-// const highlightActions = document.querySelectorAll('[data-action="highlight"]');
-// console.log(highlightActions);
-// console.log(highlightActions[0].dataset);
-// highlightActions[0].dataset.containing = "unique";
-// highlightActions[0].dataset.caseSensitive = "true";
-// for(let a of highlightActions){
-//   a.addEventListener('click',evt=>{
-//     evt.preventDefault();
-//     highlightParas(a.dataset.containing);
-//     // highlightParas('unique');
-//   });
-// }
-// const removeHighlightActions = document.querySelectorAll('[data-action="removeHighlights"]');
-// for (let a of removeHighlightActions){
-//   a.addEventListener('click', evt=>{
-//     evt.preventDefault();
-//     removeParaHighlights();
-//   });
-// }
