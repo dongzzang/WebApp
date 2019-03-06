69cd3944913fe7a7180bd18695beff188fc046a9
dongzzang <dh134579@naver.com>
2019-03-06 16:05:18 +0900

    create 2

--- Day190305/simplejquery.js
+++ Day190305/simplejquery.js
@@ -1,16 +1,75 @@
+
+function colorEm(){
+  var $div = $("div");
+  var start = Math.floor(Math.random() * $div.length);
+  var end = Math.floor(Math.random() * ($div.length-start)) + start + 1;
+
+  if(end === $div.length) {end=undefined;}
+  $div.css("background","");
+  if(end){
+    $div.slice(start, end).css("background","yellow");
+  }
+  else{
+    $div.slice(start).css("background","yellow");
+  }
+  $("span").text("$('div').slice("+ start +
+      (end ? ", " +end:"")+").css('background', 'yellow');");
+}
+$("button").click(colorEm);
+
 $(document).ready(function() {
 
-  $("td").filter(function(index){
-    return index % 3 == 0;
-  }).css("background", "red");
+  $("tr").css("background-color", function(index, value){
+    if(value == "rgb(255, 255, 0)") {return "red";}
+  });
+  // $("td").css("background","blue");
+  // $("li").each(function(){
+  //   $(this).click(function(){
+  //     $(this).addClass("selected");
+  //     $(this).siblings().removeClass("selected");
+  //   });
+  // });
+
+  // div 집합 내용에서 p요소를 가져온다.
+  // $("div").find("p").css("border", "1px solid red");
+
+  // div 집합에서 p를 찾는다
+  // $("div").filter("p").css("border", "1px solid red");
+
+  // $("div").filter(".my").css("border", "1px solid red");
+  // $("ul").find("li").contents().remove("ul");
+
+  // $("li").add("div > p").css("border", "1px solid red");
+
+  // var litext = $("li").map(function (index){
+  //   return "[("+index+")" +$(this).text() +"]";
+  // }).get().join(',');
+  // $("#result").text(litext);
+
+  // $("*").each(function(){
+  //   if($(this).is("li")){
+  //     $(this).css("border", "1px solid red");
+  //   }
+  // });
+
+  // $("li").has("ul").css("background", "red");
+
+  // $("td").not(function(index){
+  //   return index % 3 == 0;
+  // }).css("background", "red");
+
+  // $("td").filter(function(index){
+  //   return index % 3 == 0;
+  // }).css("background", "red");
+
   // $("td:nth-child(2n+1)").css("background", "red");
   // $("td:first").css("background","red");
   // $("td:first-child").css("background","red");
   // $("td:last-child").css("background", "yellow");
 
-  $("li").css("margin", "5px");
-  $(":checked,:selected").css("border", "2px solid red");
-  $(":selected").css("background","red");
+  // $("li").css("margin", "5px");
+  // $(":checked,:selected").css("border", "2px solid red");
+  // $(":selected").css("background","red");
 
   // $("#txt:focus").css("background", "blue");
   // $("tr:not(.no)").css("background","#AAA");
